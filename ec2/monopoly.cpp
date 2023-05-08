2d075b0fb0ff1ffa4cbd19c545011b8f70efc881
Caleb Brock <caleb.brock@broshin.com>
2023-05-08 16:50:14 -0700

    Actually finished ec2

--- ec2/monopoly.cpp
+++ ec2/monopoly.cpp
@@ -6,78 +6,305 @@
 #include <stdexcept>
 #include <limits>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
 
 using namespace std;
 
-#include "../cbrock.cpp"
+namespace cbrock {
 
-using namespace cbrock;
+	enum AsciiColor {
+		CLEAR = 0,
+		BOLD = 1,
+		UNDERLINE = 4,
+		BLACK = 30,
+		RED = 31,
+		GREEN = 32,
+		YELLOW = 33,
+		BLUE = 34,
+		MAGENTA = 35,
+		CYAN = 36,
+		WHITE = 37,
+	};
 
+	class FancyText {
+	private:
+		int color;
+		void setColor(int value) {
+			this->color = value;
+		};
+	public:
+		FancyText(int color) {
+			this->setColor(color);
+		};
+		int getColor() const {
+			return this->color;
+		};
+		friend ostream& operator<<(ostream& out, const FancyText& fancyText) {
+			out << "\033[" << to_string(fancyText.getColor()) << "m";
+			return out;
+		}
+	};
 
-// used to cache the amount of times a square is landed upon
-/* template <typename K, typename V> class Map {
-private:
-	struct Bucket {
-		K key;
-		V value;
-		Bucket* next = nullptr;
-		Bucket(K key, V value) {
-			this->key = key;
-			this->value = value;
+	class Assert {
+	private:
+		static int tests;
+		static int failures;
+	public:
+		template <typename T> static void assertEquals(T expected, T actual, string text = "") {
+			tests++;
+			if (expected != actual) {
+				failures++;
+				cout << FancyText(AsciiColor::RED) << "Test failure!" << endl;
+				if (!text.empty()) {
+					cout << "Failed test: " << endl;
+					cout << text << endl;
+				}
+				cout << "Expected value: " << expected << endl
+					 << "Actual value: " << actual << FancyText(AsciiColor::CLEAR) << endl;
+			}
+		};
+		static void assertTrue(bool value, string text = "") {
+			assertEquals(true, value, text);
+		};
+		static void analyze() {
+			cout << "Tests ran: " << Assert::tests << endl;
+			cout << "Test failures: " << Assert::failures << endl;
+		}
+	};
+
+	int Assert::tests = 0;
+	int Assert::failures = 0;
+
+	// This is a dynamically allocated array class
+	template <typename T> class ArrayList {
+	private:
+		T *arr;
+		int size;
+
+		// Copies the contents from src to dest arrays up to len values
+		static void CopyArr(const T *src, T *dest, int len) {
+			for (int i = 0; i < len; i++) {
+				dest[i] = src[i];
+			}
+		}
+
+	public:
+		ArrayList() {
+			arr = nullptr;
+			size = 0;
+		}
+		ArrayList(const T* startingArr, int size) {
+			arr = new T[size];
+			CopyArr(startingArr, arr, size);
+			this->size = size;
+		}
+		~ArrayList() {
+			delete[] arr;
+		}
+
+		int length() const {
+			return this->size;
+		}
+
+		bool isEmpty() const {
+			return length() == 0;
+		}
+
+		// Get the value at a point in the array
+		T at(int index) const {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			return *(arr + index);
+		}
+
+		void put(int index, T value) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+
+			*(arr + index) = value;
+		}
+
+		bool contains(T a) const {
+			for (int i = 0; i < length(); i++) {
+				if (at(i) == a)
+					return true;
+			}
+			return false;
+		}
+
+		// Get the pointer at the index in the array
+		const T* ptrAt(int index) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			return arr + index;
+		}
+
+		// Get the array if you want to for some reason
+		const T* getArray() {
+			return this->arr;
+		}
+
+		// Push a new value to the array
+		void push(T value) {
+			T *newArr = new T[size + 1];
+			// Only copy array if it has any contents
+			if (length() > 0) {
+				CopyArr(arr, newArr, size);
+			}
+			newArr[size] = value;
+			size++;
+			T *tmp = arr;
+			arr = newArr;
+			delete[] tmp;
+		}
+
+		// Removes an element at the array
+		void remove(int index) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			T *newArr = new T[size - 1];
+			CopyArr(arr, newArr, index);
+			CopyArr(arr + index + 1, newArr + index, length() - index - 1);
+
+			T *pTmp = arr;
+			arr = newArr;
+			size--;
+			delete[] pTmp;
+		}
+
+		static void ComponentTest() {
+			ArrayList<int>* a = new ArrayList<int>();
+			a->push(10);
+			a->push(13);
+			a->push(-1);
+			a->push(256);
+
+			Assert::assertEquals(4, a->length(), "Ensure created ArrayList has correct size");
+
+			a->remove(2);
+			Assert::assertEquals(3, a->length(), "Ensure deleting elements from ArrayList decreases the size");
+
+			Assert::assertTrue(a->at(0) == 10 && a->at(1) == 13 && a->at(2) == 256, "Ensure new array list is as expected");
+
+			a->put(1, 999);
+			Assert::assertEquals(999, a->at(1), "Check that setting the second element of the ArrayList changes the value");
 		};
 	};
-	Bucket** buckets;
-	int size;
-	static const int max_size = 32;
-	static int hash(K key) {
-		return (key * 17) % max_size;
-	}
-public:
-	Map() {
-		size = 0;
-		buckets = new Bucket*[max_size];
-		for (int i = 0; i < max_size; i++) {
-			buckets[i] = nullptr;
-		}
-	};
-	V get(K key) {
-		int index = hash(key);
-		if (buckets[index] != nullptr) {
-			if (buckets[index]->key == key) {
-				return buckets[index]->value;
-			} else {
-				Bucket* ptr = buckets[index];
-				while (ptr->next != nullptr) {
-					ptr = ptr->next;
-					if (ptr->key == key) {
-						return ptr->value;
-					}
-				}
-			}
-		}
-		throw invalid_argument("No such key exists in the map!");
-	};
-	void put(K key, V value) {
-		Bucket* bucket = new Bucket(key, value);
-		int index = hash(key);
-		if (buckets[index] != nullptr) {
-			Bucket* ptr = buckets[index];
-			while (ptr->next != nullptr) {
-				ptr = ptr->next;
-			}
-			ptr->next = bucket;
-		} else {
-			buckets[index] = bucket;
-		}
 
-		size++;
-	};
-	int getSize() {
-		return size;
-	}
+	class Date {
+	private:
+		int day, month, year;
+		void setMonth(int value) {
+			this->month = value;
+		};
+		void setDay(int value) {
+			this->day = value;
+		};
+		void setYear(int value) {
+			this->year = value;
+		};
+	public:
+		Date() {
+			time_t time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+			tm t = *localtime(&time);
+			this->SetDate(t.tm_mon + 1, t.tm_mday, t.tm_year + 1900);
+		};
+		Date(int month, int day, int year) {
+			this->SetDate(month, day, year);
+		};
+		void SetDate(int month, int day, int year) {
+			this->month = month;
+			this->day = day;
+			this->year = year;
+		};
+		int getMonth() const {
+			return this->month;
+		};
+		int getDay() const {
+			return this->day;
+		};
+		int getYear() const {
+			return this->year;
+		};
+		Date& operator=(const Date& other) {
+			setMonth(other.getMonth());
+			setDay(other.getDay());
+			setYear(other.getYear());
+			return *this;
+		};
+		bool operator==(const Date& other) const {
+			return getMonth() == other.getMonth() && getDay() == other.getDay() && getYear() == other.getYear();
+		};
+		friend istream& operator>>(istream& in, Date& date) {
+			int m, d, y;
+			char garbage;
+			in >> m >> garbage >> d >> garbage >> y;
+			date.SetDate(m, d, y);
+			return in;
+		};
+		friend ostream& operator<<(ostream& out, const Date& date) {
+			out << setfill('0') << setw(2) << date.getMonth()
+				<< "/" << setfill('0') << setw(2) << date.getDay()
+				<< "/" << setfill('0') << setw(4) << date.getYear();
+			return out;
+		};
+		static void ComponentTest() {
+			Date* other = new Date();
+			other->SetDate(4, 20, 2023);
+			stringstream testStream;
+			testStream << *other;
 
-};
-*/
+			Assert::assertTrue(other->month == 4 && other->year == 2023 && other->day == 20, "Check that date values are set properly");
+
+			Assert::assertTrue(testStream.str() == "04/20/2023", "Check that the date output matches the format \"MM/DD/YYYY\"");
+			delete other;
+		}
+	};
+
+	class RandNo {
+	private:
+		static RandNo* instance;
+		int seed;
+		RandNo(int seed = 0) {
+			this->seed = seed;
+			srand(this->seed);
+		};
+		// Returns a random float between 0.0 and 1.0
+		float rand01() const {
+			return (static_cast<float>(rand()) / static_cast<float>(numeric_limits<float>::max()));
+		}
+	public:
+		static const RandNo getInstance() {
+			if (instance == nullptr) {
+				instance = new RandNo(time(0));
+			}
+
+			return *instance;
+		};
+		int getRandomNumber(int min, int max) const {
+			if (min > max) {
+				throw invalid_argument("The minimum value must be below the maximum value!");
+			}
+			return ((rand() % (max - min)) + min);
+		};
+		float getRandomNumber(float min, float max) const {
+			if (min > max) {
+				throw invalid_argument("The minimum value must be below the maximum value!");
+			}
+			return (rand01() * (min - max) + min);
+		}
+	};
+
+	RandNo* RandNo::instance = nullptr;
+}
+
+using namespace cbrock;
 
 int rollDie();
 template<typename T> void shuffle(ArrayList<T>*);
@@ -166,18 +393,16 @@ public:
 	}
 };
 
-enum CardType {
-	MOVE_TO, ADVANCE, MOVE_TO_TYPE
-};
-
+// Virtual class for
 class Card {
 public:
-	virtual ~Card() = default;;
+	virtual ~Card() = default;
 	virtual int getAmountToAdvance(const Board& board) {
 		return 0;
 	};
 };
 
+// This is a card that represents just advancing a certain amount of squares. Example: go back 3 squares.
 class AdvanceCard : public Card {
 private:
 	int squaresToAdvance;
@@ -190,6 +415,7 @@ public:
 	}
 };
 
+// This is a card that represents going to a particular square. Example: go to jail, boardwalk, etc.
 class GoToCard : public Card {
 private:
 	int target;
@@ -207,6 +433,7 @@ public:
 	};
 };
 
+// This is a card that represent cards like "go to the nearest railroad/utility"
 class GoToTypeCard : public Card {
 private:
 	LocationType locationType;
@@ -234,7 +461,7 @@ public:
 
 
 int main() {
-	cout << "Welcome to the monopoly sim" << endl;
+	cout << "Welcome to the monopoly sim!" << endl;
 
 	ArrayList<Card*>* chanceDeck = new ArrayList<Card*>();
 
@@ -277,9 +504,7 @@ int main() {
 		board->playTurn();
 	}
 
-#ifdef DEBUG
 	board->outputData(cout);
-#else
 	ofstream results("results.csv");
 	if (!results.is_open()) {
 		cerr << "Couldn't open results.csv file!" << endl;
@@ -290,9 +515,6 @@ int main() {
 	results.close();
 
 	cout << "Finished writing to file!" << endl;
-#endif
-
-	delete board;
 
 	for (int i = 0; i < chanceDeck->length(); i++) {
 		delete chanceDeck->at(i);
@@ -300,6 +522,8 @@ int main() {
 	for (int i = 0; i < communityChest->length(); i++) {
 		delete communityChest->at(i);
 	}
+
+	delete board;
 
 	return 0;
 }
@@ -310,7 +534,7 @@ int rollDie() {
 
 void Board::playCards() {
 
-	ArrayList<Card*>* deck = nullptr;
+	ArrayList<Card*>* deck;
 
 	switch (map[currentPosition].locationType) {
 		case LocationType::CHANCE:
@@ -329,7 +553,7 @@ void Board::playCards() {
 
 	int toAdvance = card->getAmountToAdvance(*this);
 	advance(toAdvance);
-};
+}
 
 // This shuffle's an ArrayList's values. The values should be sufficiently random.
 template<typename T> void shuffle(ArrayList<T>* arr) {
