d118156cc54d88b6ad76205c2cf63fed2954bb4a
Caleb Brock <caleb.brock@broshin.com>
2023-05-08 15:15:21 -0700

    Added header, included cbrock namespace in file

--- final/robots.cpp
+++ final/robots.cpp
@@ -1,14 +1,6 @@
-/**
- * Todo: Remove debug defines and includes
- */
-
-#define BATTERY_POWER_GAINED 5
-#define BATTERY_POWER_LOST_PER_MOVE 1
-#define BATTERY_SPAWN_RATE 0.8f
-
-#ifdef DEBUG
-#include <bitset>
-#endif
+// robots.cpp
+// Caleb Brock, CISP 400
+// May 18, 2023
 
 #include <iostream>
 #include <cstring>
@@ -16,12 +8,305 @@
 #include <fstream>
 #include <functional>
 #include <regex>
+#include <iomanip>
+#include <chrono>
 
+// I hated typing this over and over
 typedef unsigned int Gene;
 
 using namespace std;
 
-#include "../cbrock.cpp"
+namespace cbrock {
+
+	enum AsciiColor {
+		CLEAR = 0,
+		BOLD = 1,
+		UNDERLINE = 4,
+		BLACK = 30,
+		RED = 31,
+		GREEN = 32,
+		YELLOW = 33,
+		BLUE = 34,
+		MAGENTA = 35,
+		CYAN = 36,
+		WHITE = 37,
+	};
+
+	class FancyText {
+	private:
+		int color;
+		void setColor(int value) {
+			this->color = value;
+		};
+	public:
+		FancyText(int color) {
+			this->setColor(color);
+		};
+		int getColor() const {
+			return this->color;
+		};
+		friend ostream& operator<<(ostream& out, const FancyText& fancyText) {
+			out << "\033[" << to_string(fancyText.getColor()) << "m";
+			return out;
+		}
+	};
+
+	class Assert {
+	private:
+		static int tests;
+		static int failures;
+	public:
+		template <typename T> static void assertEquals(T expected, T actual, string text = "") {
+			tests++;
+			if (expected != actual) {
+				failures++;
+				cout << FancyText(AsciiColor::RED) << "Test failure!" << endl;
+				if (!text.empty()) {
+					cout << "Failed test: " << endl;
+					cout << text << endl;
+				}
+				cout << "Expected value: " << expected << endl
+					 << "Actual value: " << actual << FancyText(AsciiColor::CLEAR) << endl;
+			}
+		};
+		static void assertTrue(bool value, string text = "") {
+			assertEquals(true, value, text);
+		};
+		static void analyze() {
+			cout << "Tests ran: " << Assert::tests << endl;
+			cout << "Test failures: " << Assert::failures << endl;
+		}
+	};
+
+	int Assert::tests = 0;
+	int Assert::failures = 0;
+
+	// This is a dynamically allocated array class
+	template <typename T> class ArrayList {
+	private:
+		T *arr;
+		int size;
+
+		// Copies the contents from src to dest arrays up to len values
+		static void CopyArr(const T *src, T *dest, int len) {
+			for (int i = 0; i < len; i++) {
+				dest[i] = src[i];
+			}
+		}
+
+	public:
+		ArrayList() {
+			arr = nullptr;
+			size = 0;
+		}
+		ArrayList(const T* startingArr, int size) {
+			arr = new T[size];
+			CopyArr(startingArr, arr, size);
+			this->size = size;
+		}
+		~ArrayList() {
+			delete[] arr;
+		}
+
+		int length() const {
+			return this->size;
+		}
+
+		bool isEmpty() const {
+			return length() == 0;
+		}
+
+		// Get the value at a point in the array
+		T at(int index) const {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			return *(arr + index);
+		}
+
+		void put(int index, T value) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+
+			*(arr + index) = value;
+		}
+
+		bool contains(T a) const {
+			for (int i = 0; i < length(); i++) {
+				if (at(i) == a)
+					return true;
+			}
+			return false;
+		}
+
+		// Get the pointer at the index in the array
+		const T* ptrAt(int index) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			return arr + index;
+		}
+
+		// Get the array if you want to for some reason
+		const T* getArray() {
+			return this->arr;
+		}
+
+		// Push a new value to the array
+		void push(T value) {
+			T *newArr = new T[size + 1];
+			// Only copy array if it has any contents
+			if (length() > 0) {
+				CopyArr(arr, newArr, size);
+			}
+			newArr[size] = value;
+			size++;
+			T *tmp = arr;
+			arr = newArr;
+			delete[] tmp;
+		}
+
+		// Removes an element at the array
+		void remove(int index) {
+			if (index < 0 || index >= length()) {
+				throw invalid_argument("Index out of bounds!");
+			}
+			T *newArr = new T[size - 1];
+			CopyArr(arr, newArr, index);
+			CopyArr(arr + index + 1, newArr + index, length() - index - 1);
+
+			T *pTmp = arr;
+			arr = newArr;
+			size--;
+			delete[] pTmp;
+		}
+
+		static void ComponentTest() {
+			ArrayList<int>* a = new ArrayList<int>();
+			a->push(10);
+			a->push(13);
+			a->push(-1);
+			a->push(256);
+
+			Assert::assertEquals(4, a->length(), "Ensure created ArrayList has correct size");
+
+			a->remove(2);
+			Assert::assertEquals(3, a->length(), "Ensure deleting elements from ArrayList decreases the size");
+
+			Assert::assertTrue(a->at(0) == 10 && a->at(1) == 13 && a->at(2) == 256, "Ensure new array list is as expected");
+
+			a->put(1, 999);
+			Assert::assertEquals(999, a->at(1), "Check that setting the second element of the ArrayList changes the value");
+		};
+	};
+
+	class Date {
+	private:
+		int day, month, year;
+		void setMonth(int value) {
+			this->month = value;
+		};
+		void setDay(int value) {
+			this->day = value;
+		};
+		void setYear(int value) {
+			this->year = value;
+		};
+	public:
+		Date() {
+			time_t time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+			tm t = *localtime(&time);
+			this->SetDate(t.tm_mon + 1, t.tm_mday, t.tm_year + 1900);
+		};
+		Date(int month, int day, int year) {
+			this->SetDate(month, day, year);
+		};
+		void SetDate(int month, int day, int year) {
+			this->month = month;
+			this->day = day;
+			this->year = year;
+		};
+		int getMonth() const {
+			return this->month;
+		};
+		int getDay() const {
+			return this->day;
+		};
+		int getYear() const {
+			return this->year;
+		};
+		Date& operator=(const Date& other) {
+			setMonth(other.getMonth());
+			setDay(other.getDay());
+			setYear(other.getYear());
+			return *this;
+		};
+		bool operator==(const Date& other) const {
+			return getMonth() == other.getMonth() && getDay() == other.getDay() && getYear() == other.getYear();
+		};
+		friend istream& operator>>(istream& in, Date& date) {
+			int m, d, y;
+			char garbage;
+			in >> m >> garbage >> d >> garbage >> y;
+			date.SetDate(m, d, y);
+			return in;
+		};
+		friend ostream& operator<<(ostream& out, const Date& date) {
+			out << setfill('0') << setw(2) << date.getMonth()
+				<< "/" << setfill('0') << setw(2) << date.getDay()
+				<< "/" << setfill('0') << setw(4) << date.getYear();
+			return out;
+		};
+		static void ComponentTest() {
+			Date* other = new Date();
+			other->SetDate(4, 20, 2023);
+			stringstream testStream;
+			testStream << *other;
+
+			Assert::assertTrue(other->month == 4 && other->year == 2023 && other->day == 20, "Check that date values are set properly");
+
+			Assert::assertTrue(testStream.str() == "04/20/2023", "Check that the date output matches the format \"MM/DD/YYYY\"");
+			delete other;
+		}
+	};
+
+	class RandNo {
+	private:
+		static RandNo* instance;
+		int seed;
+		RandNo(int seed = 0) {
+			this->seed = seed;
+			srand(this->seed);
+		};
+		// Returns a random float between 0.0 and 1.0
+		float rand01() const {
+			return (static_cast<float>(rand()) / static_cast<float>(numeric_limits<float>::max()));
+		}
+	public:
+		static const RandNo getInstance() {
+			if (instance == nullptr) {
+				instance = new RandNo(time(0));
+			}
+
+			return *instance;
+		};
+		int getRandomNumber(int min, int max) const {
+			if (min > max) {
+				throw invalid_argument("The minimum value must be below the maximum value!");
+			}
+			return ((rand() % (max - min)) + min);
+		};
+		float getRandomNumber(float min, float max) const {
+			if (min > max) {
+				throw invalid_argument("The minimum value must be below the maximum value!");
+			}
+			return (rand01() * (min - max) + min);
+		}
+	};
+
+	RandNo* RandNo::instance = nullptr;
+}
 
 using namespace cbrock;
 
@@ -375,11 +660,6 @@ public:
 		sensorState = sensorState | (east << 0x2);
 		sensorState = sensorState | west;
 
-#ifdef DEBUG
-		cout << north << " " << south << " " << east << " " << west << endl;
-		cout << "Bits: " << bitset<8>(sensorState) << endl;
-#endif
-
 		return getInstruction(sensorState);
 	};
 	void compute(Map& map) {
@@ -410,11 +690,11 @@ public:
 		}
 		if (val == Square::BATTERY) {
 			map.setSquare(newPosition, Square::NOTHING);
-			setPower(getPower() + BATTERY_POWER_GAINED);
+			setPower(getPower() + 5);
 			powerHarvested++;
 		}
 
-		setPower(getPower() - BATTERY_POWER_LOST_PER_MOVE);
+		setPower(getPower() - 1);
 		turnsSurvived++;
 	};
 
@@ -474,7 +754,7 @@ int main(int argc, char** argv) {
 		map->setStartSquare(Tuple(5, 5));
 	}
 
-	map->placeBatteries(BATTERY_SPAWN_RATE, true);
+	map->placeBatteries(0.6f, true);
 
 	cout << "Map: " << endl << *map << endl;
 
@@ -517,7 +797,7 @@ int main(int argc, char** argv) {
 	for (int generation = 0; generation < generations; generation++) {
 		int harvested = 0;
 		for (int i = 0; i < robots->length(); i++) {
-			map->placeBatteries(BATTERY_SPAWN_RATE, true);
+			map->placeBatteries(0.6f, true);
 			Robot *robot = robots->at(i);
 			while (robot->getPower() > 0) {
 				robot->compute(*map);
@@ -525,9 +805,6 @@ int main(int argc, char** argv) {
 			harvested += robot->getPowerHarvested();
 		}
 		outputData->push(Tuple(generation, harvested));
-#ifdef DEBUG
-		cout << "Robots of generation " << generation << " harvested " << harvested << " power!" << endl;
-#endif
 		if ((generation > 0 && generation % 100 == 0) || generation == generations - 1) {
 			cout << "Generation up to " << generation << " finished..." << endl;
 		}
